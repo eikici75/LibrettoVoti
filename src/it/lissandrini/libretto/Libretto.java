ceafbbd3e69088ab22457a80450ec684cc80811f
matteol <matteol@WLGESM003.GRITTIGAS.CORP>
2020-04-08 14:54:42 +0200

    Classe Libretto vuota

--- src/it/lissandrini/libretto/Libretto.java
+++ src/it/lissandrini/libretto/Libretto.java
@@ -1,5 +1,19 @@
 package it.lissandrini.libretto;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	public void add(Voto v) {
+		
+	}
 
 }
