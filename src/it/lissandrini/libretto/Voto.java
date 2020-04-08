bd648ac0aec062a4f243b9c103a72fca8bb74436
matteol <matteol@WLGESM003.GRITTIGAS.CORP>
2020-04-08 13:15:35 +0200

    Progetto iniziale. Classe Voto

---
+++ src/it/lissandrini/libretto/Voto.java
@@ -0,0 +1,11 @@
+package it.lissandrini.libretto;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class Voto {
+	private int voto;
+	private String corso;
+	private LocalDate data;
+
+}
